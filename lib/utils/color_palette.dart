64d0059455bea2dec594e7d54471229c951e4eb4
ronishbrh <ronish.brh111@gmail.com>
2026-07-01 10:51:17 +0545

    color palette utility, static const

---
+++ lib/utils/color_palette.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class ColorPalette {
+  static const Color kBgColor = Color(0xFFFFF8F7);
+  static const Color kPrimaryColor = Color(0xFF5C0510);
+  static const Color kSecondaryTextColor = Color(0xFF605E58);
+  static const Color kBorderColor = Color(0x4DDDC0BE);
+}
