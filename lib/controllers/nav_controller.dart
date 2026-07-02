c259e4e74072baee9325c6a976db924084324298
ronishbrh <109803505+ronishbrh@users.noreply.github.com>
2026-07-02 10:34:59 +0545

    Delete lib/controllers/nav_controller.dart

--- lib/controllers/nav_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-class NavController extends GetxController {
-  final selectedIndex = 1.obs; 
-
-  void changeTab(int index) {
-    selectedIndex.value = index;
-  }
-}
