3772544a2aae869c062049a282dca782237337de
ronishbrh <ronish.brh111@gmail.com>
2026-07-02 11:17:26 +0545

    import get

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:handicraftmobilefrontend/view/main_layout.dart';
 import 'package:handicraftmobilefrontend/view/ShopView.dart';
 
